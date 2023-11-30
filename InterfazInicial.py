5750f828a5bd26c7d664c468a39ca95bd4d861e5
HectorGaSa <hectorgarciasala20@gmail.com>
2023-11-30 12:40:03 +0100

    Error de las partidas arreglado

--- InterfazInicial.py
+++ InterfazInicial.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 import os
 from PIL import Image, ImageTk
 import shutil
+from tkinter import messagebox
 
 
 if not os.path.exists("bbdd"):
@@ -15,11 +16,12 @@ var_path_BD = os.path.join("bbdd", "users.db")
 
 dir = os.getcwd()
 
-directori_fotos = os.path.join(dir,"profile_images")
+directori_fotos = os.path.join(dir, "profile_images")
 if not os.path.exists(directori_fotos):
     os.makedirs(directori_fotos)
 
 contador = 0
+comprobador = 0
 
 cur_BD = var_BD.cursor()
 
@@ -33,16 +35,6 @@ cur_BD.execute('''CREATE TABLE IF NOT EXISTS jugadors (
 
 var_BD.commit()
 
-def seleccionar_imatge(imagen):
-    tipus_arxius = (
-        ('Tots els arxius', '*.*'),
-        ('Imatges tipus jpg', '*.jpg')
-    )
-    imagen = filedialog.askopenfilename(
-        initialdir=directori_fotos,
-        title='Selecciona una imatge',
-        filetypes=tipus_arxius
-    )
 def seleccionar_imagen(entry_avatar):
     directori_fotos = "profile_images"
 
@@ -86,22 +78,10 @@ def crea_usuari():
     entry_avatar_button = Button(ventana_usuari, text='Seleccionar i Obrir Imatge', command=lambda:seleccionar_imagen(entry_avatar))
     entry_avatar_button.grid(row=2, column=2)
 
-    label_games = Label(ventana_usuari, text="Partides jugades:")
-    label_games.grid(row=3, column=0)
-    entry_games = Entry(ventana_usuari)
-    entry_games.grid(row=3, column=1)
-
-    label_win = Label(ventana_usuari, text="Partides guanyades:")
-    label_win.grid(row=4, column=0)
-    entry_win = Entry(ventana_usuari)
-    entry_win.grid(row=4, column=1)
-
     entries = [
         entry_nick,
         entry_password,
-        entry_avatar,
-        entry_games,
-        entry_win]
+        entry_avatar]
 
     btn_afegir = Button(
         ventana_usuari, text="Afegir a la Base de Dades", command=lambda: afegir_a_bd(entries))
@@ -110,8 +90,6 @@ def crea_usuari():
     entry_nick.delete(0, END)
     entry_password.delete(0, END)
     entry_avatar.delete(0, END)
-    entry_games.delete(0, END)
-    entry_win.delete(0, END)
 
 
 def afegir_a_bd(datos):
@@ -122,20 +100,15 @@ def afegir_a_bd(datos):
     nick = datos[0].get()
     password = datos[1].get()
     avatar = datos[2].get()
-    games = datos[3].get()
-    win = datos[4].get()
 
     cur_BD.execute("INSERT INTO jugadors VALUES (:z_nick, :z_password, :z_avatar, :z_games, :z_win)",
-                   {'z_nick': nick, 'z_password': password, 'z_avatar': avatar, 'z_games': games, 'z_win': win})
+                   {'z_nick': nick, 'z_password': password, 'z_avatar': avatar, 'z_games': 0, 'z_win': 0})
 
     var_BD.commit()
 
     datos[0].delete(0, END)
     datos[1].delete(0, END)
     datos[2].delete(0, END)
-    datos[2].delete(0, END)
-    datos[3].delete(0, END)
-    datos[4].delete(0, END)
 
 
 def comprovar_dades():
@@ -154,6 +127,7 @@ def comprovar_dades():
 
 def comprovar_usuari(nick_entry, password_entry, frame: Frame, titulo_text):
     global contador
+    global comprobador
 
     var_BD = sqlite3.connect(var_path_BD)
     cur_BD = var_BD.cursor()
@@ -169,28 +143,166 @@ def comprovar_usuari(nick_entry, password_entry, frame: Frame, titulo_text):
         if (contador == 3):
             print("ALERTA: Ya lo has intentado el limite de veces permitida!")
             finestra.destroy()
-    else:
-        cur_BD.execute("SELECT nick, avatar FROM jugadors where nick= '" +
+    else: 
+        cur_BD.execute("SELECT nick, avatar, games, win, rowid FROM jugadors where nick= '" +
                    nick_entry + "' and password= '" + password_entry + "' ;")
         
         resultats = cur_BD.fetchall()
         for widget in frame.winfo_children():
             widget.destroy()
+        
         tit_frame = Label(frame, anchor="center", text=titulo_text)
         tit_frame.grid(row=0, column=0)
+
         nick_frame = Label(frame, anchor="center", text=nick_entry)
         nick_frame.grid(row=1, column=0)
+
+        if (resultats[0][2] != 0):
+            resultado = (resultats[0][3] / resultats[0][2]) * 100
+            wins_frame = Label(frame, anchor="center", text=str(round(resultado)) + "%")
+            wins_frame.grid(row=2, column=0)
+
         ruta_destino = os.path.join(directori_fotos, resultats[0][1])
         imagen = Image.open(ruta_destino)
-        imagenFinal = ImageTk.PhotoImage(imagen)
-        avatar_frame = Label(frame, anchor="center", image=imagenFinal)
-        avatar_frame.grid(row=2, column=0)
+        avatar_frame = Label(frame, anchor="center")
+        avatar_frame.grid(row=3, column=0)
         avatar_frame.img = ImageTk.PhotoImage(imagen)
         avatar_frame.config(image=avatar_frame.img)
+
+        btn_delete = Button(frame, text="Borrar usuario", command=lambda: borrarUsuario(resultats[0][4], frame, titulo_text))
+        btn_delete.grid(row=4, column=0)
+
+        btn_modificar = Button(frame, text="Modificar", command=lambda:modificarUsuario(nick_entry, imagen, resultats[0][1], password_entry, resultats[0][4]))
+        btn_modificar.grid(row=5, column=0)
+
         contador = 0
+        comprobador += 1
+
+        activarBoton()
 
 
     var_BD.close()
+
+
+def modificarUsuario(usuario, imagen, foto, password, id):
+    ventana_modificar = Toplevel()
+    ventana_modificar.title("Modificar usuario")
+
+    label_nick = Label(ventana_modificar, text="Nick: ")
+    label_nick.grid(row=1, column=0)
+
+
+    var = StringVar(ventana_modificar)
+    var.set(usuario)
+    entry_nick = Entry(ventana_modificar, textvariable=var)
+    entry_nick.grid(row=1, column=1)
+
+    btn_mod_nick = Button(ventana_modificar, text="Actualizar nick", command=lambda:modicarNick(entry_nick.get(), usuario, id))
+    btn_mod_nick.grid(row=1, column=2)
+
+    label_password = Label(ventana_modificar, text="Nueva contraseña: ")
+    label_password.grid(row=2, column=0)
+
+    entry_password = Entry(ventana_modificar)
+    entry_password.grid(row=2, column=1)
+
+    btn_mod_password = Button(ventana_modificar, text="Actualizar contraseña", command=lambda:modificarPassword(entry_password.get(), password))
+    btn_mod_password.grid(row=2, column=2)
+
+    label_avatar = Label(ventana_modificar, text="Avatar: ")
+    label_avatar.grid(row=3, column=0)
+
+    label_imagen = Label(ventana_modificar, anchor="center")
+    label_imagen.grid(row=3, column=1) 
+
+    label_imagen.img = ImageTk.PhotoImage(imagen)
+    label_imagen.config(image=label_imagen.img)
+
+    btn_mod_avatar = Button(ventana_modificar, text="Cambiar avatar", command=lambda:modificarAvatar(foto, label_imagen))
+    btn_mod_avatar.grid(row=3, column=2)
+
+    btn_cerrar = Button(ventana_modificar, text="Cerrar ventana", command=ventana_modificar.destroy)
+    btn_cerrar.grid(row=5, column=1)
+
+
+def modicarNick(usuarioNew, usuarioOld, id):
+    var_BD = sqlite3.connect(var_path_BD)
+    cur_BD = var_BD.cursor()
+    cur_BD.execute("UPDATE jugadors set nick = '" + usuarioNew + "' where nick = '" + usuarioOld + "' and rowid = '" + str(id) + "';")
+    var_BD.commit()
+    var_BD.close()
+
+def modificarPassword(passwordNew, passwordOld):
+    var_BD = sqlite3.connect(var_path_BD)
+    cur_BD = var_BD.cursor()
+    cur_BD.execute("UPDATE jugadors set password = '" + passwordNew + "' where password = '" + passwordOld + "';")
+    var_BD.commit()
+    var_BD.close()
+
+
+def modificarAvatar(avatarOld, labelImagen):
+    directori_fotos = "profile_images"
+
+    if not os.path.exists(directori_fotos):
+        os.makedirs(directori_fotos)
+    
+    ruta_imagen = filedialog.askopenfilename(title="Seleccionar imagen", filetypes=[("Archivos de imagen", "*.png;*.jpg;*.jpeg;*.gif")])
+    nombre_archivo = os.path.basename(ruta_imagen)
+
+    ruta_destino = os.path.join(directori_fotos, nombre_archivo)
+
+    try:
+        if ruta_imagen != ruta_destino:
+            shutil.copy2(ruta_imagen, ruta_destino)
+    except Exception as e:
+        print(f"Error al copiar la imagen: {e}")
+
+
+    var_BD = sqlite3.connect(var_path_BD)
+    cur_BD = var_BD.cursor()
+    cur_BD.execute("UPDATE jugadors set avatar = '" + nombre_archivo + "' where avatar = '" + avatarOld + "';")
+    var_BD.commit()
+    var_BD.close()
+
+    imagenChange = Image.open(ruta_destino)
+    labelImagen.img = ImageTk.PhotoImage(imagenChange)
+    labelImagen.config(image=labelImagen.img)
+
+
+
+def borrarUsuario(id, frame, titulo):
+    var_BD = sqlite3.connect(var_path_BD)
+    cur_BD = var_BD.cursor()
+    confirmacion = messagebox.askyesno("Confirmar", "¿Estás seguro de que quieres borrar?")
+    if confirmacion:
+        cur_BD.execute("DELETE from jugadors where rowid = '" + str(id) + "';")
+        var_BD.commit()
+        print("Borrado realizado")  # Aquí puedes poner la lógica de borrado o lo que desees
+    var_BD.close()
+    for widget in frame.winfo_children():
+            widget.destroy()
+    tit_frame = Label(frame, anchor="center", text=titulo)
+    tit_frame.grid(row=0, column=0)
+    label_nick = Label(frame, anchor="center", text="Nick:")
+    label_nick.grid(row=1, column=0)
+    entry_nick = Entry(frame, justify="center")
+    entry_nick.grid(row=2, column=0)
+
+    label_password = Label(frame, anchor="center", text="Contrasenya:")
+    label_password.grid(row=3, column=0)
+    entry_password = Entry(frame, justify="center")
+    entry_password.grid(row=4, column=0)
+
+    btn_entra_usuari = Button(
+        frame, text="Entrar", command=lambda:comprovar_usuari(entry_nick.get(), entry_password.get(), frame, tit_frame.cget("text")))
+    btn_entra_usuari.grid(row=5, column=0, columnspan=2)
+
+def activarBoton():
+    global comprobador
+    if (comprobador == 2):
+        btn_inici.configure(state=NORMAL)
+    else:
+        btn_inici.configure(state=DISABLED)
 
 
 
@@ -212,7 +324,7 @@ entry_password1 = Entry(frame_j1, justify="center")
 entry_password1.grid(row=4, column=0)
 
 btn_entra_usuari1 = Button(
-    frame_j1, text="Entrar", command=lambda: comprovar_usuari(entry_nick1.get(), entry_password1.get(),frame_j1, tit_frame1.cget("text")))
+    frame_j1, text="Entrar", command=lambda:comprovar_usuari(entry_nick1.get(), entry_password1.get(), frame_j1, tit_frame1.cget("text")))
 btn_entra_usuari1.grid(row=5, column=0, columnspan=2)
 
 tit_frame2 = Label(frame_j2, anchor="center", name="jugador 2", text="Jugador 2:")
@@ -243,6 +355,10 @@ btn_crea_usuari.grid(row=6, column=0, columnspan=2)
 btn_comprovar = Button(
     finestra, text="Comprovar Dades", command=comprovar_dades)
 btn_comprovar.grid(row=7, column=0, columnspan=2)
+
+btn_inici = Button(
+    finestra, text="Inciar joc", state=DISABLED)
+btn_inici.grid(row=6, column=2, columnspan=2)
 
 finestra.geometry("600x600")
 finestra.mainloop()
